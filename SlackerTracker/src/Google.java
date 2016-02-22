ac0d27660d9c0ee6aeb58428662240f2af95aa07
Brian Woestman <woestmab@gmail.com>
2016-02-21 23:39:13 -0600

    testing some http request ideas with google

--- SlackerTracker/src/Google.java
+++ SlackerTracker/src/Google.java
@@ -1,69 +1,18 @@
-import com.google.api.client.http.HttpTransport;
-import com.google.api.client.http.LowLevelHttpRequest;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.JsonGenerator;
-import com.google.api.client.json.JsonParser;
-import com.google.maps.*;
-import com.sun.deploy.net.HttpResponse;
+import android.util.Log;
+import com.google.maps.GeoApiContext;
+import com.sun.xml.internal.txw2.Document;
+import org.apache.log4j.Logger;
 
-import org.apache.http.client.methods.HttpGet;
-
-import java.io.*;
-import java.nio.charset.Charset;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * Created by Bdub on 2/19/16.
  */
 public class Google
 {
-    private static final HttpTransport HTTP_TRANSPORT = new HttpTransport()
-    {
-        @Override
-        protected LowLevelHttpRequest buildRequest(String method, String url) throws IOException
-        {
-            return null;
-        }
-    };
-
-    static final JsonFactory JSON_FACTORY = new JsonFactory()
-    {
-        @Override
-        public JsonParser createJsonParser(InputStream in) throws IOException
-        {
-            return null;
-        }
-
-        @Override
-        public JsonParser createJsonParser(InputStream in, Charset charset) throws IOException
-        {
-            return null;
-        }
-
-        @Override
-        public JsonParser createJsonParser(String value) throws IOException
-        {
-            return null;
-        }
-
-        @Override
-        public JsonParser createJsonParser(Reader reader) throws IOException
-        {
-            return null;
-        }
-
-        @Override
-        public JsonGenerator createJsonGenerator(OutputStream out, Charset enc) throws IOException
-        {
-            return null;
-        }
-
-        @Override
-        public JsonGenerator createJsonGenerator(Writer writer) throws IOException
-        {
-            return null;
-        }
-    };
-
     private static final String GOOGLE_API_KEY = "AIzaSyBKXLe2ChocA8-es3OiIGlKcPnFtELUBH4\t\n";
 
     public static void main(String[] args)
@@ -74,14 +23,36 @@ public class Google
         geoApiContext.setApiKey(GOOGLE_API_KEY);
 
         //test addresses
-        String origin = "75 9th Ave, New York, NY";
-        String destination = "MetLife Stadium Dr East Rutherford, NJ 07073";
+        String origin = "Madison";
+        String destination = "Milwaukee";
+        String urlString = "http://maps.googleapis.com/maps/api/directions/json?sensor=false&origin="+origin+"&destination="+destination;
 
-        //create a request
-//        DirectionsApiRequest directionsApiRequest =
-//                DirectionsApi.getDirections(geoApiContext, origin, destination);
+        try
+        {
+            URL googleService = new URL(urlString);
 
-        DirectionsApiRequest directionsApiRequest1 = DirectionsApi.newRequest(geoApiContext);
+            HttpURLConnection googleConnection = (HttpURLConnection) googleService.openConnection();
+
+            googleConnection.setAllowUserInteraction(false);
+            googleConnection.setDoInput(true);
+            googleConnection.setDoOutput(false);
+            googleConnection.setUseCaches(true);
+            googleConnection.setRequestMethod("GET");
+            googleConnection.connect();
+
+            DocumentBuilderFactory factoryDir = DocumentBuilderFactory.newInstance();
+            DocumentBuilder parseDirInfo = factoryDir.newDocumentBuilder();
+            org.w3c.dom.Document docDir = parseDirInfo.parse(googleConnection.getInputStream());
+
+            googleConnection.disconnect();
+            String result = docDir.toString();
+
+            System.out.println(result);
+
+        }
+        catch (Exception e)
+        {
+            System.out.println(e);
+        }
     }
-
 }
